f58281e9346db87bd8dd037b42168ad988b3220d
Kyle <kylgrg@gmail.com>
2021-09-02 10:56:55 -0500

    Concurrent State Machine Port

---
+++ Zeta-Repository/CSM.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zeta.Generics;
+
+namespace Zeta.CSM
+{
+    public class StateMachine
+    {
+        public const string Splitter = "_";
+        public const string Default = "Default";
+        public const string Start = "Start";
+
+
+        Rolodex State;
+        Dictionary<string, Node> TotalNodes;
+        Dictionary<string, List<(string node, string gate)>> Connections;
+        public HashSet<string> ActiveNodes;
+
+        List<string> QuedMessages;
+
+        public StateMachine()
+        {
+            State = new Rolodex();
+            TotalNodes = new Dictionary<string, Node>();
+            Connections = new Dictionary<string, List<(string node, string gate)>>();
+            ActiveNodes = new HashSet<string>();
+
+            QuedMessages = new List<string>();
+        }
+
+        public bool Push<T>(string name, Func<T> get, Action<T> set) => State.Push(name, get, set);
+        public bool Push<T>(string name, T t) => State.Push(name, t);
+
+        public void ProcessMessages()
+        {
+            foreach (var message in QuedMessages)
+            {
+                var splitMessage = message.Split(new string[] { Splitter }, StringSplitOptions.None);
+                if (splitMessage.Length >= 2)
+                    if (TotalNodes.TryGetValue(splitMessage[0], out var n)) { n.Exit(State, splitMessage[1]); ActiveNodes.Remove(n.Name); }
+                if (Connections.TryGetValue(message, out var v))
+                {
+                    foreach (var target in v)
+                    {
+                        if (TotalNodes.TryGetValue(target.node, out var tNode))
+                        {
+                            tNode.Enter(State, target.gate);
+
+                            ActiveNodes.Add(target.node);
+                        }
+                    }
+                }
+            }
+            QuedMessages.Clear();
+        }
+
+        public void SendMessage(string message)
+        {
+            QuedMessages.Add(message);
+        }
+
+        public void SendFromGate(string node, string gate = Default)
+        {
+            SendMessage(node + Splitter + gate);
+        }
+
+        public void AddNode(Node node)
+        {
+            if (!TotalNodes.TryGetValue(node.Name, out var v)) { Nodes.Add(node.Name, node); node.Initialize(this); return; }
+            throw new System.InvalidOperationException($"A node with name {node.Name} already present in State Machine.");
+        }
+        public bool RemoveNode(string name)
+        {
+            return TotalNodes.Remove(name);
+        }
+
+        public void AddConnection(string outNode, string outGate, string inNode, string inGate = Default)
+        {
+            var key = outNode + Splitter + outGate;
+            if (Connections.TryGetValue(key, out var l)) { l.Add((inNode, inGate)); return; }
+            var newL = new List<(string, string)>();
+            newL.Add((inNode, inGate));
+            Connections.Add(key, newL);
+        }
+        public void RemoveConnection(string outNode, string outGate, string inNode, string inGate = Default)
+        {
+            var key = outNode + Splitter + outGate;
+            if (Connections.TryGetValue(key, out var l))
+            {
+                l.Remove((inNode, inGate));
+                if (l.Count == 0)
+                {
+                    Connections.Remove(key);
+                }
+            }
+        }
+        public void AddMessageListener(string message, string inNode, string inGate = Default)
+        {
+            if (Connections.TryGetValue(message, out var l)) { l.Add((inNode, inGate)); return; }
+            var newL = new List<(string, string)>();
+            newL.Add((inNode, inGate));
+            Connections.Add(message, newL);
+        }
+        public void RemoveMessageListener(string message, string inNode, string inGate)
+        {
+            if (Connections.TryGetValue(message, out var l))
+            {
+                l.Remove((inNode, inGate));
+                if (l.Count == 0)
+                {
+                    Connections.Remove(message);
+                }
+            }
+        }
+
+    }
+    public class Node
+    {
+        public string Name;
+        protected Rolodex Stack;
+        StateMachine Machine;
+
+        public delegate void NodeAction(Rolodex state, Rolodex stack);
+        public List<NodeAction> EnterActions;
+        public List<NodeAction> ExitActions;
+        public List<NodeAction> UpdateActions;
+        public List<NodeAction> FixedUpdateActions;
+
+        public Dictionary<string, NodeAction> IGateActions = new Dictionary<string, NodeAction>();
+        public Dictionary<string, NodeAction> OGateActions = new Dictionary<string, NodeAction>();
+
+        public Node(string name, StateMachine machine)
+        {
+            Name = name;
+
+            Stack.Push("Name", () => Name, (d) => Name = d);
+            Stack.Push("Node", this);
+
+            Machine = machine;
+        }
+
+        public virtual void OnEnter(Rolodex state)       { }
+        public virtual void OnUpdate(Rolodex state)      { }
+        public virtual void OnFixedUpdate(Rolodex state) { }
+        public virtual void OnExit(Rolodex state)        { }
+
+        public void Enter(Rolodex state, string iGate = StateMachine.Default)
+        {
+            if (IGateActions.TryGetValue(iGate, out var v)) v.Invoke(state, Stack);
+
+            foreach (var action in EnterActions) { action.Invoke(state, Stack); }
+
+            OnEnter(state);
+        }
+        public void Update(Rolodex state)
+        {
+            foreach(var action in UpdateActions) { action.Invoke(state, Stack); }
+
+            OnUpdate(state);
+        }
+        public void FixedUpdate(Rolodex state)
+        {
+            foreach (var action in FixedUpdateActions) { action.Invoke(state, Stack); }
+
+            OnFixedUpdate(state);
+        }
+        public void Exit(Rolodex state, string oGate = StateMachine.Default)
+        {
+            if (OGateActions.TryGetValue(oGate, out var v)) v.Invoke(state, Stack);
+
+            foreach (var action in ExitActions) { action.Invoke(state, Stack); }
+
+            OnExit(state);
+        }
+
+        public void SendFromGate(string gate = StateMachine.Default) => Machine.SendFromGate(Name, gate);
+        public void SendMesssage(string message) => Machine.SendMessage(message);
+
+
+        public enum ActionType { ENTER, EXIT, UPDATE, FIXEDUPDATE, IGATE, OGATE }
+        public const ActionType EnterType = ActionType.ENTER;
+        public const ActionType ExitType = ActionType.EXIT;
+        public const ActionType UpdateType = ActionType.UPDATE;
+        public const ActionType FixedUpdateType = ActionType.FIXEDUPDATE;
+
+        public Node AddBaseAction(ActionType type, NodeAction action)
+        {
+            Action<NodeAction> addMethod = null;
+            switch (type)
+            {
+                case EnterType:
+                    addMethod = AddEnterAction;
+                    break;
+                case ExitType:
+                    addMethod = AddExitAction;
+                    break;
+                case UpdateType:
+                    addMethod = AddUpdateAction;
+                    break;
+                case FixedUpdateType:
+                    addMethod = AddFixedUpdateAction;
+                    break;
+            }
+            addMethod?.Invoke(action);
+            return this;
+        }
+        public Node AddBaseActions(ActionType type, params NodeAction[] actions)
+        {
+            foreach (var action in actions) AddBaseAction(type, action);
+            return this;
+        }
+        public Node SetIGateAction(string iGate, NodeAction action)
+        {
+            SetIGate(iGate, action);
+            return this;
+        }
+        public Node SetOGateAction(string oGate, NodeAction action)
+        {
+            SetOGate(oGate, action);
+            return this;
+        }
+
+        void AddEnterAction(NodeAction action) { EnterActions.Add(action); }
+        void AddExitAction(NodeAction action) { ExitActions.Add(action); }
+        void AddUpdateAction(NodeAction action) { UpdateActions.Add(action); }
+        void AddFixedUpdateAction(NodeAction action) { FixedUpdateActions.Add(action); }
+        void SetIGate(string iGate, NodeAction action) { if (!IGateActions.TryGetValue(iGate, out var a)) IGateActions.Add(iGate, action); }
+        void SetOGate(string oGate, NodeAction action) { if (!OGateActions.TryGetValue(oGate, out var a)) OGateActions.Add(oGate, action); }
+
+    }
+}
