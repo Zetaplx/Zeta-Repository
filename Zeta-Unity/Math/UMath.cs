747424cc0bddd1c9d6779c67cb28e3b04c2b1e14
Kyle <kylgrg@gmail.com>
2021-09-05 18:39:47 -0500

    Added extended vector capabilities, primarily vector2 rotations

--- Zeta-Unity/Math/UMath.cs
+++ Zeta-Unity/Math/UMath.cs
@@ -42,4 +42,22 @@ namespace Zeta.Math.Unity
             return subs.ToArray();
         }
     }
+
+    public static class UVectorUtils
+    {
+        public static Vector2 RotateVector2(Vector2 v, float angle, AngleMode angleMode = AngleMode.DEGREES)
+        {
+            float trueAngle = angle * (angleMode == AngleMode.DEGREES ? Mathf.Deg2Rad : 1);
+            return new Vector2(v.x * Mathf.Cos(trueAngle) - v.y * Mathf.Sin(trueAngle), v.x * Mathf.Sin(trueAngle) + v.y * Mathf.Cos(trueAngle));
+        }
+    }
+
+    public static class UVectorExtensions
+    {
+        public static void Rotate(this ref Vector2 v, float angle, AngleMode mode = AngleMode.DEGREES) 
+            => v = UVectorUtils.RotateVector2(v, angle, mode);
+
+        public static Vector2 Rotated(this Vector2 v, float angle, AngleMode mode = AngleMode.DEGREES)
+            => UVectorUtils.RotateVector2(v, angle, mode);
+    }
 }
---
+++ Zeta.Tests/UMathTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using UnityEngine;
+using Zeta.Math.Unity;
+
+namespace Zeta.Tests
+{
+    
+    
+    public class UMathTest
+    {
+
+        [Fact]
+        public void Test1 ()
+        {
+            Vector2 v = new Vector2(1, 0);
+            v.Rotate(90);
+            Assert.True((v - new Vector2(0, 1)).sqrMagnitude < 0.05f);
+            v.Rotate(90);
+            Assert.True((v - new Vector2(-1, 0)).sqrMagnitude < 0.05f);
+            v.Rotate(90);
+            Assert.True((v - new Vector2(0, -1)).sqrMagnitude < 0.05f);
+            v.Rotate(90);
+            Assert.True((v - new Vector2(1, 0)).sqrMagnitude < 0.05f);
+            v.Rotate(-90);
+            Assert.True((v - new Vector2(0, -1)).sqrMagnitude < 0.05f);
+        }
+    }
+}
